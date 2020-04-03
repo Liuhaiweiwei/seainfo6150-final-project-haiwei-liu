b48b53b6268054cbc2287743c9d886d22dfb3a2c
Haiwei liu <liuhaiweiwei@gmail.com>
2020-04-02 17:21:07 -0700

    week 13 assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./Home/homepage.jsx";
+import Error from "./Error/Error.jsx";
+import About from "./About/About.jsx";
+import Tags from "./Tags/Tags";
+import Follow from "./Follow/Follow";
+import Blogs from "./Blogs/Blogs";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/blogs/Btitle">Blogs</Link>
+                </li>
+                <li>
+                    <Link to="/tags">Tags</Link>
+                </li>
+                <li>
+                    <Link to="/about">About Me</Link>
+                </li>
+                <li>
+                    <Link to="/follow">Follow Me</Link>
+                </li>
+            </ul>
+        </nav>
+      </div>
+        <Switch>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/about" exact component={About}></Route>
+            <Route path="/tags" exact component={Tags}></Route>
+            <Route path="/follow" exact component={Follow}></Route>
+            <Route path="/blogs/:Btitle"
+                   exact
+                   render={({ match }) =>(
+                       <Blogs
+                           Btitle={match.params.Btitle}
+                       />
+                   )}>
+            </Route>
+            <Route component={Error}></Route>
+        </Switch>
+    </Router>
   );
 }
 
